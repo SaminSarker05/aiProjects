4ddd27a1d09d8af73155aa5c204805160872fd78
Samin Sarker <saminsarker05@gmail.com>
2025-11-17 22:08:22 -0500

    renamed file

---
+++ monkey_agent/main.py
@@ -0,0 +1,120 @@
+"""
+Langchain/LangGraph MultiAgent coding assistant using openai GPT-4.
+
+to use:
+    define a .env file with OPENAI_API_KEY
+    run uv sync
+    run uv run main.py
+"""
+
+from dotenv import load_dotenv
+from typing import Annotated, Literal, TypedDict, List
+from prompts import planner_agent_prompt, architect_agent_prompt, coder_agent_prompt
+from schemas import *
+from utils import *
+from langgraph.graph import StateGraph, END
+from langgraph.prebuilt import create_react_agent
+from langchain_openai import ChatOpenAI
+
+load_dotenv()
+
+# low temperature for consistent results
+llm = ChatOpenAI(model='gpt-5-nano-2025-08-07', temperature=0.1)
+
+def planner_monkey(state: AgentState) -> AgentState:
+    """
+    Generate comprehensive project plan based on the user prompt.
+    """
+    user_prompt: str = state["user_prompt"]
+    prompt = planner_agent_prompt(user_prompt)
+    response: PlanSchema = llm.with_structured_output(PlanSchema).invoke(prompt)
+    if response is None:
+        raise Exception("Planner Agent: Unable to generate plan.")
+    
+    return {"plan": response}
+
+def architect_monkey(state: AgentState) -> AgentState:
+    """
+    Break down plan into actionaable tasks with implementation details.
+    """
+    plan_str = str(state["plan"])
+    prompt = architect_agent_prompt(plan_str)
+    response = llm.with_structured_output(ArchitectSchema).invoke(prompt)
+    if response is None:
+        raise Exception("Architect Agent: Unable to generate tasks.")
+    
+    return {"architect": response}
+
+def code_monkey(state: AgentState) -> AgentState:
+    """
+    Execute each implementation task one by one.
+    """
+    coder_state = state.get("coder_state")
+    if coder_state is None:
+        coder_state = CoderState(architect=state["architect"], curr_task_ind=0)
+    
+    # get current task and existing code from coder_state
+    list_of_tasks = coder_state.architect.tasks
+    if coder_state.curr_task_ind >= len(list_of_tasks):
+        return {"coder_state": coder_state, "status": "DONE"}
+
+    curr_task = list_of_tasks[coder_state.curr_task_ind]    
+    existing_code = read_file.run(curr_task.path)
+    user_prompt = (
+        f"task: {curr_task.task_description}"
+        f"file: {curr_task.path}"
+        f"existing_code: {existing_code}"
+        "use write_file(path, content) to modify the file"
+        "ensure code is clean, well-commented, and follows best practices"
+        
+    )
+    system_prompt = coder_agent_prompt()
+    
+    monkey_tools = [read_file, write_file, ls_files, get_cwd]
+    agent = create_react_agent(model=llm,  tools=monkey_tools)
+    # pass in general system prompt and user prompt
+    agent.invoke({"messages": [{"role": "system", "content": system_prompt}, {"role": "user", "content": user_prompt} ]})
+    
+    # increment curr_task_ind and return status
+    coder_state.curr_task_ind += 1
+    return {"coder_state": coder_state, "status": "IN_PROGRESS"}
+
+def build_graph():
+    """
+    Build state graph for monkey software engineer agent.
+    """
+    graph = StateGraph(AgentState)
+    graph.add_node("plan_monkey", planner_monkey)
+    graph.add_node("architect_monkey", architect_monkey)
+    graph.add_node("code_monkey", code_monkey)
+
+    graph.set_entry_point("plan_monkey")
+    graph.add_edge("plan_monkey", "architect_monkey")
+    graph.add_edge("architect_monkey", "code_monkey")
+
+    # allow recursion of code_monkey node
+    graph.add_conditional_edges(
+        "code_monkey",
+        lambda state: "END" if state.get("status") == "DONE" else "code_monkey",
+        {"END": END, "code_monkey": "code_monkey"}
+    )
+    return graph.compile()
+
+def main():
+    try:
+        swe_agent = build_graph()
+        user_prompt = input("what do you want to build?: ")
+        res = swe_agent.invoke({"user_prompt": user_prompt}, recursion_limit=100)
+        
+        final_status = res["status"]
+        if final_status == "DONE":
+            print("Project built successfully!")
+        else:
+            print("Project build failed.")
+    except KeyboardInterrupt:
+        print("Exiting...")
+    except Exception as e:
+        print(f"Error: {e}")
+
+if __name__ == "__main__":
+    main()
---
+++ monkey_agent/schemas.py
@@ -0,0 +1,43 @@
+from pydantic import BaseModel, Field, ConfigDict
+from typing import Literal, TypedDict, Optional
+
+class FileSchema(BaseModel):
+    path: str = Field(description="Location and path of file to create or modify.")
+    purpose: str = Field(description="Purpse of this file.")
+
+class PlanSchema(BaseModel):
+    name: str = Field(description="Name of application to build.")
+    description: str = Field(description="One sentence description of application to build.")
+    tech_stack : str = Field(description="The tech stack that will be used to build the application.")
+    features: list[str] = Field(description="List of features that will be included in the application.")
+    files: list[FileSchema] = Field(description="List of files to create or modify during the build process.")
+    
+    # used for passing plan to architect agent/monkey
+    def __str__(self):
+        return f"""
+            name: {self.name}
+            description: {self.description}
+            tech Stack: {self.tech_stack}
+            features: {self.features}
+            files: {self.files}
+        """
+    
+class TaskSchema(BaseModel):
+    path: str = Field(description="The path of file to modify.")
+    task_description: str = Field(description="Detailed description of the task to perform on the file.")
+    
+class ArchitectSchema(BaseModel):
+    tasks: list[TaskSchema] = Field(description="List of tasks to perform during the build process.")
+
+class CoderState(BaseModel):
+    architect: ArchitectSchema = Field(description="List of implementation tasks to perform during the build process.")
+    curr_task_ind: int = Field(0, description="Index of current task being worked on.")
+    curr_file_content: Optional[str] = Field(None, description="Current file content being worked on.")
+
+class AgentState(TypedDict):
+    user_prompt: str
+    plan: PlanSchema
+    architect: ArchitectSchema
+    coder_state: Optional[CoderState]
+    status: Literal["DONE", "IN_PROGRESS", "ERROR"]
+    
